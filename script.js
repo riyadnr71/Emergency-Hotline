9aaf5237cd727a0d993f663624cb1ce64aed8c4e
riyadnr71 <riyadprodhan91@gmail.com>
2025-08-28 21:54:41 +0600

    added countheart

---
+++ script.js
@@ -0,0 +1,50 @@
+
+
+
+function heartCount(id){
+
+    document.getElementById(id).addEventListener('click', function(){
+        totalHeartCount +=1;
+        clickCount.innerHTML= totalHeartCount
+        return;
+    })
+}
+
+
+heartCount('heart');
+heartCount('heart1');
+heartCount('heart2');
+heartCount('heart3');
+heartCount('heart4');
+heartCount('heart5');
+heartCount('heart6');
+heartCount('heart7');
+heartCount('heart8');
+
+
+
+
+
+const  clickCount = document.getElementById('countheart')
+
+
+    let totalHeartCount = 0;
+
+
+
+
+
+
+document.getElementById("call-btn").addEventListener('click', function(){
+
+    
+    console.log('nisa i love you sona')
+})
+
+
+
+
+
+
+
+
