4c6a1d73dbde6d3c594e1d9d5fdfbb3cb75893ad
riyadnr71 <riyadprodhan91@gmail.com>
2025-08-29 00:56:23 +0600

    added fotter

--- script.js
+++ script.js
@@ -11,6 +11,12 @@ function heartCount(id){
 }
 
 
+const  clickCount = document.getElementById('countheart')
+
+
+    let totalHeartCount = 0;
+
+
 heartCount('heart');
 heartCount('heart1');
 heartCount('heart2');
@@ -25,11 +31,70 @@ heartCount('heart8');
 
 
 
-const  clickCount = document.getElementById('countheart')
 
 
-    let totalHeartCount = 0;
+//   alert function
 
+function ShowAlert (serviceNameId, serviceNumberId){
+
+     const serviceName = document.getElementById(serviceNameId).innerText;
+
+    const serviceNumber = document.getElementById(serviceNumberId).innerText;
+
+     const alertMessage = 'Calling'+ ' ' + serviceName + ' ' + serviceNumber;
+
+
+
+    alert(alertMessage)
+
+    return alert
+
+
+    
+
+}
+
+
+
+// coin 
+
+function decCoin(){
+    const coin2 = document.getElementById('coin-balance')
+
+     let coin1 = parseInt(coin2.innerText);
+
+    let newCoin = coin1 - 20
+
+
+
+    if(newCoin >=0){
+
+        coin2.innerText = newCoin
+
+        ShowAlert('serviceName', 'serviceNumber')
+
+    }
+
+    else{
+        alert('আপনার পর্যাপ্ত কয়েন নেই!কল করতে কমপক্ষে 20 টি কয়েক লাগবে।')
+    }
+    return
+
+}
+
+
+
+    
+
+
+
+
+
+    
+
+
+
+    
 
 
 
@@ -37,8 +102,61 @@ const  clickCount = document.getElementById('countheart')
 
 document.getElementById("call-btn").addEventListener('click', function(){
 
+
+     ShowAlert('serviceName', 'serviceNumber')
+     decCoin()
     
-    console.log('nisa i love you sona')
+    
+    // const coin2 = document.getElementById('coin-balance')
+
+    // let coin1 = parseInt(coin2.innerText);
+
+    // let newCoin = coin1 - 20
+
+    // coin2.innerHTML = newCoin
+
+    // if(newCoin < 0 ){
+    //     alert('আপনার পর্যাপ্ত কয়েন নেই!কল করতে কমপক্ষে 20 টি কয়েক লাগবে।')
+
+
+    // } 
+    
+    
+    
+
+ 
+    
+})
+
+
+
+document.getElementById("police-btn").addEventListener('click', function(){
+
+    ShowAlert('serviceName1', 'serviceNumber2')
+
+
+    if(coin1 =0 ){
+        alert('আপনার পর্যাপ্ত কয়েন নেই!কল করতে কমপক্ষে 20 টি কয়েক লাগবে।')
+    }
+   
+
+    let coin1 = parseInt(coin2.innerText);
+
+    let newCoin = coin1 -20
+
+    coin2.innerHTML = newCoin
+
+    
+
+
+    
+
+
+    
+    
+
+ 
+    
 })
 
 
